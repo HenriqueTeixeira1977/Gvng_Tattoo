6885de0723d230343faa07b9279ca58ef2e413da
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2025-03-26 19:28:05 -0300

    20250326192537-Alterações no index.html, head.php, header.php, no includes e inicio da pagina studio.php em pages;

--- includes/header.php
+++ includes/header.php
@@ -1,29 +1,107 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Henriq Tatto</title>
-  
-  <!-- Link do Bootstrap 5 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
-  
-  <!-- Vendor CSS Files -->
-  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="../vendor/aos/aos.css" rel="stylesheet">
-  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-  <link href="../vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="../vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Gang | Custom Art</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
 
-  <link rel="stylesheet" href="../css/styles.css">
-
-
-
-
-
+    <!-- Favicons -->
+    <link href="../assets/images/favicon.png" rel="icon">
+    <link href="../assets/images/apple-touch-icon.png" rel="apple-touch-icon">
+    
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    
+    <!-- Vendor CSS Files -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../vendor/aos/aos.css" rel="stylesheet">
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="../vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="../vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    
+    <!-- Main CSS File -->
+    <link href="../css/main.css" rel="stylesheet">
 </head>
 <body>
+  <header id="header" class="header d-flex align-items-center fixed-top">
+      <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+          <div id="logotipo" class="logotipo">
+              <!-- Uncomment the line below if you also wish to use an image logo -->
+              <div id="logo-img" class="logo-img">
+                  <a href="#"><img src="../assets/images/logotipos/logo4.png" alt=""></a>
+              </div>        
+              <div id="logo-txt" class="logo-txt">
+                  <a href="#" class="titulo">Gang</a>
+                  <!--  <a href="index.php" class="slogan">Lettering Tattoo</a>  -->
+              </div>
+          </div>
+          <nav id="navmenu" class="navmenu">
+              <ul>
+                  <li><a href="#" class="active">Início</a></li>
+                  <li><a href="#projects">PROJETOS</a></li>
+
+                  <li><a href="#features">CARACTERISTICAS</a></li>
+                  <li class="dropdown"><a href="#services"><span>ESTILOS</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                      <ul>
+                          <li><a href="#">ESTILO1</a></li>
+                              <li class="dropdown"><a href="#"><span>ESTILO2</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                                  <ul>
+                                      <li><a href="#">ESTILO3</a></li>
+                                      <li><a href="#">ESTILO4</a></li>
+                                      <li><a href="#">ESTILO5</a></li>
+                                  </ul>
+                              </li>
+                          <li><a href="#">STYLE1</a></li>
+                          <li><a href="#">STYLE2</a></li>
+                          <li><a href="#">STYLE3</a></li>
+                          <li><a href="#">STYLE4</a></li>
+                      </ul>
+                  </li>
+                  <li class="dropdown"><a href="#alt-services"><span>STUDIO</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                      <ul>
+                          <li><a href="#cases">ALLANA</a></li>
+                          <li><a href="#projects">PETTER</a></li>
+                          <li><a href="#testimonials">JOHN</a></li>
+                          <li><a href="#recent-blog-posts">MARY</a></li>              
+                      </ul>
+                  </li>
+                  <li><a href="#cases">Cases</a></li>
+                  <li><a href="#testimonials">TESTEMUNHOS</a></li>
+                  <li><a href="#recent-blog-posts">BLOG</a></li>              
+                  <li><a href="#about">SOBRE</a></li>
+                  <li><a href="#get-started">CONTATO</a></li>
+              </ul>
+              <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          </nav>
+      </div>
+  </header>
+        <!-- Effects  -->
+
+    <!-- Scroll Top -->
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Preloader -->
+    <div id="preloader"></div>
+
+    <!-- Vendor JS Files -->
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../vendor/php-email-form/validate.js"></script>
+    <script src="../vendor/aos/aos.js"></script>
+    <script src="../vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="../vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+    <script src="../vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="../vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="../vendor/purecounter/purecounter_vanilla.js"></script>
+    
+    <!-- Main JS File -->
+    <script src="../js/main.js"></script>
+    <script src="../js/script.js"></script>
+
+    <!-- /Effects -->
+
+</body>
---
+++ pages/studio.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php include '../includes/head.php'; ?>
+<?php include '../includes/header.php'; ?>
+
+<body>
+        <!-- Projects Section -->
+        <section id="projects" class="projects section">
+
+        <!-- Section Title -->
+        <div class="container section-title" data-aos="fade-up">
+            <h2>Projects</h2>
+            <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        </div><!-- End Section Title -->
+
+        <div class="container">
+
+        <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+
+            <ul class="portfolio-filters isotope-filters" data-aos="fade-up" data-aos-delay="100">
+            <li data-filter="*" class="filter-active">All</li>
+            <li data-filter=".filter-remodeling">Remodeling</li>
+            <li data-filter=".filter-construction">Construction</li>
+            <li data-filter=".filter-repairs">Repairs</li>
+            <li data-filter=".filter-design">Design</li>
+            </ul><!-- End Portfolio Filters -->
+
+            <div class="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-remodeling">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo01.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>App 1</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo01.png" title="App 1" data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-construction">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo02.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>Product 1</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo02.png" title="Product 1" data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-repairs">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo03.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>Branding 1</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo03.png" title="Branding 1" data-gallery="portfolio-gallery-branding" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-design">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo04.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>Books 1</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo04.png" title="Branding 1" data-gallery="portfolio-gallery-book" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-remodeling">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo05.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>App 2</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo05.png" title="App 2" data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-construction">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo06.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>Product 2</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo06.png" title="Product 2" data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-repairs">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo07.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>Branding 2</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo07.png" title="Branding 2" data-gallery="portfolio-gallery-branding" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-design">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo08.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>Books 2</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo08.png" title="Branding 2" data-gallery="portfolio-gallery-book" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-remodeling">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo09.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>App 3</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo09.png" title="App 3" data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-construction">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo10.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>Product 3</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo10.png" title="Product 3" data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-repairs">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo11.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>Branding 3</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo11.png" title="Branding 2" data-gallery="portfolio-gallery-branding" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-design">
+                <div class="portfolio-content h-100">
+                <img src="assets/images/portifolio/tattoo12.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                    <h4>Books 3</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur</p>
+                    <a href="assets/images/portifolio/tattoo12.png" title="Branding 3" data-gallery="portfolio-gallery-book" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                </div>
+                </div>
+            </div><!-- End Portfolio Item -->
+
+            </div><!-- End Portfolio Container -->
+
+        </div>
+
+        </div>
+
+        </section>
+        <!-- /Projects Section -->        
+
+    
+</body>
+</html>
